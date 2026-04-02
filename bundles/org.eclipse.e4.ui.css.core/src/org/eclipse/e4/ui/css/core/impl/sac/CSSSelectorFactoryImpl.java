4c5ac0bd47a4969bff1d44a3c5545c125dfa27eb
Federico Jeanne <Federico.Jeanne@vector.com>
2026-04-02 15:36:42 +0200

    2

--- bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/sac/CSSSelectorFactoryImpl.java
+++ bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/sac/CSSSelectorFactoryImpl.java
@@ -115,7 +115,7 @@ public class CSSSelectorFactoryImpl implements SelectorFactory {
 	 */
 	@Override
 	public CharacterDataSelector createCDataSectionSelector(String data)
-			throws CSSException { // hola
+			throws CSSException { // hola 2
 		throw new CSSException(NOT_IMPLEMENTED_IN_CSS2);
 	}
 
